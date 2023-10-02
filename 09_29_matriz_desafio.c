a8c8cf23dfa1a449f8ce7a81a691fdb367e13bc6
JoviOakz <145990662+JoviOakz@users.noreply.github.com>
2023-10-02 10:37:04 -0300

    Update 09_29_matriz_desafio.c

--- 09_29_matriz_desafio.c
+++ 09_29_matriz_desafio.c
@@ -11,137 +11,170 @@ void print(char mat[3][3]){
 
         printf("|\n");
     }
+
+    printf("\n");
 }
 
 void gameplay(char mat[3][3]){
-    int cont, lin = 0, col = 0;
+    int jlin, jcol, lin, col, cont = 0, cond = 1;
 
-    while(1 != 0){
-        printf("Jogador1:\n");
-
+    while(cond != 0){
         do{
-            printf("Linha > ");
-            scanf("%d", &lin);
-            while((getchar()) != '\n');
+            printf("Jogador 1:\n\n");
 
-            if(lin < 0 || lin > 2){
-                printf("Erro!\n");
-            }
+            do{
+                printf("Linha > ");
+                scanf("%d", &jlin);
+                while((getchar()) != '\n');
+
+                if(jlin < 1 || jlin > 3){
+                    printf("Erro!\n\n");
+                }
+            }while(jlin < 1 || jlin > 3);
+
+            lin = jlin - 1;
             
-            printf("Coluna > ");
-            scanf("%d", &col);
-            while((getchar()) != '\n');
+            do{
+                printf("Coluna > ");
+                scanf("%d", &jcol);
+                while((getchar()) != '\n');
 
-            if(col < 0 || col > 2){
-                printf("Erro!\n");
-            }
+                if(jcol < 1 || jcol > 3){
+                    printf("Erro!\n\n");
+                }
+            }while(jcol < 1 || jcol > 3);
+
+            col = jcol - 1;
 
             if(mat[lin][col] == 'X' || mat[lin][col] == 'O'){
-                printf("Erro! Tente novamente\n");
+                printf("Este espaco ja esta ocupado!\n\n");
             }
-
-            if(mat[lin][col] != 'X' || mat[lin][col] != 'O'){
+            else if(mat[lin][col] != 'X' && mat[lin][col] != 'O'){
                 mat[lin][col] = 'X';
-            }
-        }while(mat[lin][col] == 'X' || mat[lin][col] == 'O');
-
-        printf("Jogador2:\n");
-
-        print(mat);
-
-        do{
-            printf("Linha > ");
-            scanf("%d", &lin);
-            while((getchar()) != '\n');
-
-            if(lin < 0 || lin > 2){
-                printf("Erro!\n");
-            }
-            
-            printf("Coluna > ");
-            scanf("%d", &col);
-            while((getchar()) != '\n');
-
-            if(col < 0 || col > 2){
-                printf("Erro!\n");
-            }
-
-            if(mat[lin][col] == 'X' || mat[lin][col] == 'O'){
-                printf("Erro! Tente novamente\n");
-            }
-
-            if(mat[lin][col] != 'X' || mat[lin][col] != 'O'){
-                mat[lin][col] = 'O';
+                break;
             }
         }while(mat[lin][col] == 'X' || mat[lin][col] == 'O');
 
         print(mat);
+        cond = end(mat, cond);
 
-        for(int i = 0; i < 3; i++){
-            if(mat[i][0] == mat[i][1] && mat[i][0] == mat[i][2]){
-                if(mat[i][0] == 'X'){
-                    print(mat);
-                    printf("Jogador1 Venceu!");
-                }
-                else{
-                    print(mat);
-                    printf("Jogador2 Venceu!");
-                }
-            }
+        if(cond == 0){
+            break;
         }
 
-        for(int i = 0; i < 3; i++){
-            if(mat[0][i] == mat[1][i] && mat[0][i] == mat[2][i]){
-                if(mat[0][i] == 'X'){
-                    print(mat);
-                    printf("Jogador1 Venceu!");
-                }
-                else{
-                    print(mat);
-                    printf("Jogador2 Venceu!");
-                }
-            }
-        }
-
-        for(int i = 0; i < 3; i++){
-            if(mat[0][0] == mat[1][1] && mat[0][0] == mat[2][2]){
-                if(mat[0][0] == 'X'){
-                    print(mat);
-                    printf("Jogador1 Venceu!");
-                }
-                else{
-                    print(mat);
-                    printf("Jogador2 Venceu!");
-                }
-            }
-        }
-
-        for(int i = 0; i < 3; i++){
-            if(mat[0][2] == mat[1][1] && mat[0][2] == mat[2][0]){
-                if(mat[0][2] == 'X'){
-                    print(mat);
-                    printf("Jogador1 Venceu!");
-                }
-                else{
-                    print(mat);
-                    printf("Jogador2 Venceu!");
-                }
-            }
-        }
-
-        cont = 0;
-
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(mat[i][j] == 'X' || mat[i][j] == 'O'){
-                    cont += 1;
-                }
-            }
-        }
+        cont += 1;
 
         if(cont == 9){
-            print(mat);
             printf("Empate!\n");
+            cond = 0;
+            break;
+        }
+
+        do{
+            printf("Jogador 2:\n\n");
+
+            do{
+                printf("Linha > ");
+                scanf("%d", &jlin);
+                while((getchar()) != '\n');
+
+                if(jlin < 1 || jlin > 3){
+                    printf("Erro!\n\n");
+                }
+            }while(jlin < 1 || jlin > 3);
+
+            lin = jlin - 1;
+            
+            do{
+                printf("Coluna > ");
+                scanf("%d", &jcol);
+                while((getchar()) != '\n');
+
+                if(jcol < 1 || jcol > 3){
+                    printf("Erro!\n\n");
+                }
+            }while(jcol < 1 || jcol > 3);
+
+            col = jcol - 1;            
+
+            if(mat[lin][col] == 'X' || mat[lin][col] == 'O'){
+                printf("Este espaco ja esta ocupado!\n\n");
+            }
+            else if(mat[lin][col] != 'X' && mat[lin][col] != 'O'){
+                mat[lin][col] = 'O';
+                break;
+            }
+        }while(mat[lin][col] == 'X' || mat[lin][col] == 'O');
+
+        print(mat);
+        end(mat, cond);
+
+        cont += 1;
+
+        if(cond == 0){
+            break;
+        }
+    }
+}
+
+int end(char mat[3][3], int cond){
+    for(int i = 0; i < 3; i++){
+        if(mat[i][0] == mat[i][1] && mat[i][0] == mat[i][2]){
+            if(mat[i][0] == 'X'){
+                printf("Jogador 1 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+            else if(mat[i][0] == 'O'){
+                printf("Jogador 2 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        if(mat[0][i] == mat[1][i] && mat[0][i] == mat[2][i]){
+            if(mat[0][i] == 'X'){
+                printf("Jogador 1 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+            else if(mat[0][i] == 'O'){
+                printf("Jogador 2 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        if(mat[0][0] == mat[1][1] && mat[0][0] == mat[2][2]){
+            if(mat[0][0] == 'X'){
+                printf("Jogador 1 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+            else if(mat[0][0] == 'O'){
+                printf("Jogador 2 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        if(mat[0][2] == mat[1][1] && mat[0][2] == mat[2][0]){
+            if(mat[0][2] == 'X'){
+                printf("Jogador 1 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
+            else if(mat[0][2] == 'O'){
+                printf("Jogador2 Venceu!\n");
+                cond = 0;
+                return cond;
+            }
         }
     }
 }
